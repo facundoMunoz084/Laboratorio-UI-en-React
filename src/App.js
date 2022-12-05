84b55b19a06b061ab701e559149869a8e07e26b7
facundoMunoz084 <munozfacundo706@gmail.com>
2022-12-04 21:51:09 -0300

    se agrega el proyecto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import { Box, Button, Flex, Heading, Stack } from '@chakra-ui/react'
+import bgImage from './resources/images/s4.jpg'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+    h='40vh'
+    bg='gray'
+    bgImage={`url(${bgImage})`}
+    bgRepeat='no-repeat'
+    bgSize='cover'
+    >
+    <Flex
+    direction='column'
+    alignItems='center'
+    justify='center'
+    h='100%'
+    bg='rgb(0 0 0 / 50%)'
+    p={['0 10%', null, '0 20%']}
+    >
+    <Heading
+    color='white'
+    textTransform='uppercase'
+    textAlign='center'
+    fontWeight='light'
+    letterSpacing='5px'
+    >domina el terreno</Heading>
+    <Stack 
+    direction={{ base: 'column', sm:'row'}} 
+    spacing='40px' 
+    mt='30px'
+    w={['100%', null, 'auto']}>
+      <Button
+        variant='outline'
+        size='lg'
+        textTransform='uppercase'
+        fontWeight='light'
+        borderRadius='0'
+        color='white'
+        letterSpacing='1px'
+        _hover={{
+          color: 'black',
+          bg: 'white'
+        }}
+        >ver detalles
+      </Button>
+      <Button
+        variant='outline'
+        size='lg'
+        textTransform='uppercase'
+        fontWeight='light'
+        borderRadius='0'
+        color='white'
+        letterSpacing='1px'
+        _hover={{
+          color: 'black',
+          bg: 'white'
+        }}
+         >ver video
+      </Button>
+    </Stack>
+    </Flex>
+    </Box>
+      
+    
   );
 }
 
